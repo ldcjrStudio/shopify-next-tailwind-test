bddce244de456309f7f65998a4f80f4c6c89f250
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-10-16 19:42:14 -0400

    Added product for ui

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -1,7 +1,23 @@
-import React from "react";
+import Image from "next/image";
+import ProductForm from "./ProductForm";
 
 const ProductPageContent = ({ product }) => {
-  return <div>{product.title}</div>;
+  return (
+    <div className="flex">
+      {product.title}
+
+      <div className="relative img-container w-72 h-72">
+        <Image
+          src={product.images.edges[0].node.originalSrc}
+          alt={product.images.edges[0].node.altText}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <ProductForm product={product} />
+    </div>
+  );
 };
 
 export default ProductPageContent;
